5c082c1c0df60f21f0ac95db6720d8ec13a8e4ab
rokaf6444 <dino1106@naver.com>
2024-07-22 16:17:59 +0900

    first commit

---
+++ backend/src/main/java/coveloper/coveloper_spring/CoveloperSpringApplication.java
@@ -0,0 +1,13 @@
+package coveloper.coveloper_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoveloperSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoveloperSpringApplication.class, args);
+	}
+
+}
